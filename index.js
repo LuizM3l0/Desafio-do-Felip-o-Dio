0c6e776fd442097cec622b43f001628c83c6b052
luizm3l0 <luizfelipemm0@gmail.com>
2023-12-21 21:46:29 -0300

    desafio concluido

---
+++ index.js
@@ -0,0 +1,25 @@
+//QUASE CUSTOU MINHA VIDA
+let nome = "Mestre das Armas"
+let nivel = 9999
+let xp = ""
+
+//Operador  if
+
+if(nivel <= 1000){
+console.log(xp = "ferro")
+}else if(nivel <= 2000){
+console.log(xp = "bronze")}
+else if(nivel <= 5000){
+console.log(xp = "Prata")}
+else if(nivel <= 7000 ){
+console.log(xp = "Ouro")}
+else if(nivel <= 8000 ){
+console.log(xp = "Platina")}
+else if(nivel <= 9000 ){
+console.log(xp = "Ascendente")}
+else if(nivel <= 10000){
+console.log(xp = "Imortal")}
+else if(nivel >= 10001){
+console.log(xp = "Radiante")}
+
+console.log("O Heroi " + nome + " " + "está no nivel " + xp)
